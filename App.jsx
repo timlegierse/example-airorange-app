cd718ff434eccaf51a7d16132ecd6e7a2eddcd86
timlegierse <118201191+timlegierse@users.noreply.github.com>
2024-08-21 15:06:15 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -26,6 +26,10 @@ export default function App() {
             name="Nic A"
             src="https://nicallen.co.uk/assets/images/image01.jpg?v=46174480"
           />
+           <Avatar
+            name="Tim L"
+            src="https://media.licdn.com/dms/image/v2/C4E03AQGI72Unhz-tmA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1605177274081?e=1729728000&v=beta&t=QJeNH8d7QzUbeatV7PVmBUCOYZM1j_syLNTY6hdgeac"
+          />
         </div>
       </div>
     </div>
