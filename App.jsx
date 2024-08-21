a53c34dc5806964d50165a9fcc76e0c315c4662d
timlegierse <118201191+timlegierse@users.noreply.github.com>
2024-08-21 15:09:38 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -6,10 +6,7 @@ export default function App() {
     <div>
       <div className="container">
         <div className="row">
-          <Avatar
-            name="David Darnes"
-            src="https://hub.darn.es/content/images/size/w4000/2022/02/dave-penfold.jpg"
-          />
+  
           <Avatar
             name="Seth Corker"
             src="https://sethcorker.com/static/web_prof-small-5ea7bafb5e58043f7536b63497995d29.jpg"
